fd73b652b63cebb43bd191da811b7e025c5c8448
RisnawatiR31 <risna3374@gmail.com>
2020-03-15 09:06:23 +0700

    Assessment 1 template boostrap

---
+++ src/components/Cards.vue
@@ -0,0 +1,64 @@
+<template>
+	<b-container>
+	<div>
+  <br><br><br><b-card-group columns>
+    <b-card
+      title="My youth"
+      img-src="https://i1.wp.com/saveasbrand.com/wp-content/uploads/2019/03/Asal-usul-Warna-Pastel-dan-Kegunannya-Dalam-Desain-Grafis-1.png?ssl=1"
+      img-alt="Image"
+      img-top
+    >
+      <b-card-text>
+        Remember, from two eyes that used to be one universe.
+      </b-card-text>
+    </b-card>
+
+    <b-card header="Quote">
+      <blockquote class="blockquote mb-0">
+        <p>There are many things I want to do. Such as greeting you and looking you in the eye.</p>
+        <footer class="blockquote-footer">
+          Run away <cite title="Source Title">~ 15 March 2020</cite>
+        </footer>
+      </blockquote>
+    </b-card>
+
+    <b-card title="Tomorrow, Today" img-src="https://i1.wp.com/saveasbrand.com/wp-content/uploads/2019/03/Asal-usul-Warna-Pastel-dan-Kegunannya-Dalam-Desain-Grafis-3.jpg?ssl=1" img-alt="Image" img-top>
+      <b-card-text>
+        Why do I pick Tomorrow, Today?
+      </b-card-text>
+      <b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>
+    </b-card>
+
+    <b-card bg-variant="info" text-variant="white">
+      <blockquote class="card-blockquote">
+        <p>If no one taks to me, Without a word<br> Without knowing, The only thing I look for is YOU</p>
+        <footer>
+          <small>Thank You <cite title="Source Title">~ 15 March 2020</cite></small>
+        </footer>
+      </blockquote>
+    </b-card>
+
+    <b-card>
+      <b-card-title>Now or Never</b-card-title>
+      <b-card-text>
+        Time is passing, Today is yesterday, Tomorrow is become Today.
+      </b-card-text>
+      <b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>
+    </b-card>
+
+    <b-card img-src="https://i0.wp.com/saveasbrand.com/wp-content/uploads/2019/03/Asal-usul-Warna-Pastel-dan-Kegunannya-Dalam-Desain-Grafis-2.png?ssl=1" img-alt="Image" overlay></b-card>
+
+    <b-card img-src="https://i0.wp.com/saveasbrand.com/wp-content/uploads/2019/03/Asal-usul-Warna-Pastel-dan-Kegunannya-Dalam-Desain-Grafis-5.jpg?ssl=1" img-alt="Image" img-top>
+      <b-card-text>
+        This content is created to fulfill the course assignment "Web Framework".
+      </b-card-text>
+      <template v-slot:footer>
+        <small class="text-muted">Risnawati</small>
+      </template>
+    </b-card>
+  </b-card-group><br><br>
+</div>
+	
+</b-container>
+</template>
+
